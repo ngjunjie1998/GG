b186a084bb784d840e2726fc877ca68d061c4ab7
JJ <ngjunjiecometj@gmail.com>
2019-09-17 09:50:58 +0800

    Delete update.php

--- update.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'Telegram.php';
-$bot_token = '980805135:AAEWOjsDBR7BX8S99TNnAHg34z9YThCarNY';
-$telegram = new Telegram($bot_token);
-$text = $telegram->Text();
-$chat_id = $telegram->ChatID();
-$content = array('chat_id' => $chat_id, 'text' => 'Hello');
-$telegram->sendMessage($content);
